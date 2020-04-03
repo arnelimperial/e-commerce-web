ebe8c9dda99accf1435154b236ff496d9d55b2f0
Arnel Imperial <engine@Arnels-MacBook-Pro.local>
2020-04-03 22:03:58 +0300

    Added stripe payment and update favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_5Qkmlyhf59pXJjEUNzfOGu1B00drxJtkXr';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return(
+        <StripeCheckout
+            label='Pay Now'
+            name='AI Data Industries'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+        
+
+    );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import { connect } from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 
 const CheckOutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -28,9 +30,13 @@ const CheckOutPage = ({ cartItems, total }) => (
         {
             cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
         }
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following for test card for payments*
+            <br/>
+            Acct. #: 4242 42424 2424 2424 - Exp: 01/20 - CVV: 123 
         </div>
+        <StripeCheckoutButton price={total} />
 
     </div>
 
